ad06c17ae73b28539383944f23ea5e211eb6c6d5
Felipe Diaz <pipexir@gmail.com>
2017-08-04 02:10:49 -0500

    fix spots problem

--- Arena/00/spots.py
+++ Arena/00/spots.py
@@ -1,36 +1,31 @@
 from sys import stdin
 
-taken = []
-for i in range(505):
-    a = []
-    for j in range(505):
-        a.append([])
-    taken.append(a)
-
-
-
-
 def main():
 
     W = 0
     H = 0
     N = 0
-    print("sss")
+
     line = stdin.readline().strip()
     data = line.split(" ")
-    print(data)
+
     W = int(data[0])
     H = int(data[1])
     N = int(data[2])
-    while ( W != 0 and H !=0 and N != 0):
-        print(data)
+    while ( W != 0 or H != 0 or N != 0):
+
+        taken = []
+        for i in range(505):
+            taken.append([False for j in range(505)])
+
+
         for i in range(N):
             X1 = 0
             X2 = 0
             Y1 = 0
             Y2 = 0
             data = stdin.readline().strip().split(" ")
-            print(data)
+
             X1 = int(data[0])
             Y1 = int(data[1])
             X2 = int(data[2])
@@ -47,17 +42,18 @@ def main():
                 Y1 =  c
 
 
-            for y in range(Y1 - 1 , Y2):
-                for x  in range( X1 - 1, X2):
-                    taken[y][x] =  True
+            for x in range(X1 - 1 , X2):
+                for y  in range( Y1 - 1, Y2):
+                    taken[x][y] =  True
+
 
         count = 0
-        for y in range( H):
-            for x in range(W):
-                if (not taken[y][x]):
-                    count+=1
+        for x in range(W):
+            for y in range(H):
+                if (taken[x][y] == True):
+                    taken[x][y] = True
                 else:
-                    taken[y][x] = False
+                    count += 1
 
         if (count == 0):
             print("There is no empty spots.")
@@ -66,10 +62,11 @@ def main():
         else:
             print("There are %d empty spots." % count)
         stdin.readline()
-        #line = stdin.readline().strip()
-        data = line.split(" ")
+
+        data = stdin.readline().split(" ")
         W = int(data[0])
         H = int(data[1])
         N = int(data[2])
+
 
 main()
