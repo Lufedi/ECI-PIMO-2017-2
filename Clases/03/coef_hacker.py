d2bf26551a0439aa4f3e1efa632da1d013d99064
Felipe Diaz <pipexir@gmail.com>
2017-09-07 23:03:33 -0500

    binomial coeficient

---
+++ Clases/03/coef_hacker.py
@@ -0,0 +1,24 @@
+from sys import  stdin
+def binomialCoef(n, k):
+    C = [[0 for x in range(k + 1)] for x in range(n + 1)]
+
+    # Calculate value of Binomial Coefficient in bottom up manner
+    for i in range(n + 1):
+        for j in range(min(i, k) + 1):
+            # Base Cases
+            if j == 0 or j == i:
+                C[i][j] = 1
+
+            # Calculate value using previosly stored values
+            else:
+                C[i][j] = C[i - 1][j - 1] + C[i - 1][j]
+
+    return C[n][k]
+
+
+# Driver program to test above function
+cases = int(stdin.readline())
+for i in range(cases):
+    n,k = stdin.readline().strip().split(" ")
+
+    print(binomialCoef(int(n), int(k)))
